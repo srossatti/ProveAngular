6ced97b04c6ef5b2cc2bdae29fb29cd376e6d9ca
srossatti <simone.rossatti@gmail.com>
2021-12-09 09:34:36 +0100

    1 file modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -8,7 +8,7 @@ export interface Product {
 export const products = [
   {
     id: 1,
-    name: 'Phone XL1',
+    name: 'Phone XL',
     price: 799,
     description: 'A large phone with one of the best screens'
   },
